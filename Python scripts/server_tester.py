50d99aeebbceb3a08d3850466c2e79e80eb7ac72
Alexandros444 <a.mohr@campus.tu-berlin.de>
2025-03-19 14:32:49 +0100

    control.sh

--- Python scripts/server_tester.py
+++ Python scripts/server_tester.py
@@ -4,10 +4,10 @@ url = "http://jserv.ddns.net:8080"
 url2 = "http://testserver.aeq-web.com/sim800_test/"
 data = {
     'Sensor0': 100,
-    'Sensor1': 200
+    'Sensor1': 200  
 }
 
-response = requests.put(url, json=data)
+response = requests.post(url, json=data)
 
 print(f"Status Code: {response.status_code}")
 print(f"Response Text: {response.text}")
