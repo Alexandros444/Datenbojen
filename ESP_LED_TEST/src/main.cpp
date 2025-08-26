e2dfc9977005dcef15c2704896d6bf2ec82f2e81
Alexandros444 <a.mohr@campus.tu-berlin.de>
2025-08-26 22:39:03 +0200

    Example function no Delay

--- ESP_LED_TEST/src/main.cpp
+++ ESP_LED_TEST/src/main.cpp
@@ -183,6 +183,33 @@ void gen_lh() {
     std::fill(lh.begin() + section1 * 2 + section2, lh.end(), 0);
 }
 
+/**
+ * i kann mit Progress ersetzt werden
+ * lastUpdate mit der Klassenvariable last Update
+ * falls es mehr als 10 funktionen sind, ansonsten is static ok speichertechnisch vernachlässigbar
+ */
+void colourFadeTest_no_delay(){
+    // Overflow to zero wenn i > 255
+    static uint8_t i = 0;
+    static unsigned long lastUpdate = 0;
+    if (millis() - lastUpdate < 20*TIMEFACTOR)
+        return;
+    lastUpdate = millis();
+    CHSV first = CHSV(96,SATURATION,MAX_BRIGHTNESS);
+    CHSV second = CHSV(72,SATURATION,MAX_BRIGHTNESS);
+    CHSV target;
+    for (;i<255;i++){
+        target = blend(first,second,i);
+        for(int dot = 0; dot < NUM_LEDS; dot++){
+            leds[dot] = target;
+        }
+        FastLED.show();
+        return;
+    }
+    // i manuell zurücksetzen, weil loop ja bei letzten wert weitermacht
+    i = 0;
+}
+
 void colourFadeTest(){
     CHSV first = CHSV(96,SATURATION,MAX_BRIGHTNESS);
     CHSV second = CHSV(72,SATURATION,MAX_BRIGHTNESS);
